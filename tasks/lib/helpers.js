6a2012faddbe2cd49e01feedb84436a58fb27416
Cristian Lorenzetto <cristian.lorenzetto@gmail.com>
2017-07-01 20:12:32 +0200

    add preprocessor enabled for default setting

--- tasks/lib/helpers.js
+++ tasks/lib/helpers.js
@@ -1,41 +1,44 @@
-
 'use strict';
 /**
-* Returns a set/get style internal storage bucket.
-*
-* @return {object} the API to set and retrieve data
-*/
-module.exports.createStore = function () {
-var bucket = {};
-var toString= function(){ return JSON.stringify(bucket); }
-/**
-* Sets a property on the store, with the given value.
-*
-* @param {string} property an identifier for the data
-* @param {*} value the value of the data being stored
-* @return {function} the set function itself to allow chaining
-*/
-var set = function (property, value) {
-bucket[property] = value;
-return set;
-};
-/**
-* Returns the store item asked for, otherwise all of the items.
-*
-* @param {string|undefined} property the property being requested
-* @return {*} the store item that was matched
-*/
-var get = function (property) {
-if (!property) {
-return bucket;
-}
-return bucket[property];
-};
-return {
-keys:function(){return Object.keys(bucket);},
-set: set,
-get: get,
-bucket:bucket
-};
+ * Returns a set/get style internal storage bucket.
+ *
+ * @return {object} the API to set and retrieve data
+ */
+module.exports.createStore = function() {
+    var bucket = {};
+    var toString = function() {
+        return JSON.stringify(bucket);
+    }
+    /**
+     * Sets a property on the store, with the given value.
+     *
+     * @param {string} property an identifier for the data
+     * @param {*} value the value of the data being stored
+     * @return {function} the set function itself to allow chaining
+     */
+    var set = function(property, value) {
+        bucket[property] = value;
+        return set;
+    };
+    /**
+     * Returns the store item asked for, otherwise all of the items.
+     *
+     * @param {string|undefined} property the property being requested
+     * @return {*} the store item that was matched
+     */
+    var get = function(property) {
+        if (!property) {
+            return bucket;
+        }
+        return bucket[property];
+    };
+    return {
+        keys: function() {
+            return Object.keys(bucket);
+        },
+        set: set,
+        get: get,
+        bucket: bucket
+    };
 
 };
